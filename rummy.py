feace78cf4661d43246acbe094a99bda3de4b355
blackbe <benjamin.black75@gmail.com>
2019-04-11 02:54:54 +0000

    added Card and Deck classes

--- rummy.py
+++ rummy.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+
+#in the python standard library
+import random
 
 """
 Author: Ben Black
@@ -44,14 +47,112 @@ cards onto a discard pile, leaving him with exactly seven
 cards in his hand.
 """
 
- 
-#Class to describe a deck of cards
-
-
+#Constants
 SUIT = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
-FACE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+FACE_VALUES = { 'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13 }
+# JOKER = { 'J*': 777 }
+
+class Card:
+    # Class to describe a card
+
+    def __init__(self, suit, value):
+        """
+        Constructor
+        Args: 
+            suit: string
+            value: character
+        Returns: none
+        """
+        self.suit = suit
+        self.value = value
+		
+    def __str__(self):
+        """
+        1.function to display the card correctly
+        Args: none
+        Returns: string representation of the card
+        """
+        return (self.suit + self.value)
+
+
+class Deck:
+    # Class to describe a deck
+
+    def __init__(self, name):
+        """
+        Constructor
+        Args: 
+            deck: number of cards in the deck - int
+        Returns: none
+        """
+        self.cards = []  #for cards in the deck
+        self.stack = []  #for the draw stack
+        self.pile = []   #for the discard pile
+        self.hand = []
+        self.name = name
+        
+    def shuffle(self):
+		""" Shuffle the Deck, so that cards are ordered in a random order
+		Args: none
+		Returns: none
+		"""
+		random.shuffle(self.cards)
+		
+    def deal_card(self, card):
+		""" Deal a Card to the Player
+		Args:
+			card:  The Card object provided to Player as part of the deal
+		Returns:
+			No returns
+		"""
+		try:
+			self.hand.append(card)
+			if len(self.hand) > 7:
+				raise ValueError('ERROR: Player cannot have more than 7 cards during turn')
+		except ValueError as err:
+			print(err.args) 
+	
+    def draw_cards(self):
+		""" Draw a card from the top of the Deck
+		Args:
+			No args
+		Returns:
+			a Card Object
+		"""
+		s = self.stack[0]
+		self.cards.pop(0)
+		return s
+
+    def discard(self):
+		""" Discard a card from the players hand
+		Args:
+			No args
+		Returns:
+			a Card Object
+		"""
+		p = self.pile[0]
+		self.cards.append(0)
+		return p
+"""
 
 
 
+Client script using classes to play rummy:
+1. hand dealt to each player - 2-4
+2. take turns drawing/improving/discard (max number of cards is 7)
+3. win conditions
+    - 4 of same suit, 3 of same suit = 00
+    - 4 of same suit, run of 3       = 01
+    - run of 4, 3 of same suit       = 10
+    - run of 4, run of 3             = 11
+4. scoring
+"""
 
 
+"""
+Items to do:
+1. Need unit tests
+2. Calculate for efficiency
+3. easy to understand
+4. use good formatting
+"""
