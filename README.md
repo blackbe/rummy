6575cac3ab7c666212f1992be75c13828b69ab34
blackbe <benjamin.black75@gmail.com>
2019-04-15 03:50:00 +0000

    Added checking and some other classes and Checked outputs and added todo items to README

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 This is a simple rummy game used to demonstrate simple object oriented design principles
 
 ## Improvements to be made
+1. The standard python standard shuffle method doesn't always contain all of the cards, so need to write own algorithm for random.shuffle to contain all 52 cards in a shuffled list that would still use the random library
+2. I used 2 for loops.   That is  O(N2) and so if a larger data set were to be used I'd want to find a different algorithm, but since the data set is small this should be fine.
+3. I need to define the game play as I didn't get that far yet.
+    
+        a. hand dealt to each player - 2-4
+        b. take turns drawing/improving/discard (max number of cards is 7)
+        c. Need to loop until win condition is met:
+    
+                i. sort the hand
+                ii. loop through hand looking for the following:
+                    - check for 3 of a kind or 3 of same suit, first...if not found exit immediately to optimize ( would be null until a condition is met)
+                    - 4 of same suit, 3 of same suit = 00
+                    - 4 of same suit, run of 3       = 01
+                    - run of 4, 3 of same suit       = 10
+                    - run of 4, run of 3             = 11
+        d. Variant - Jokers...if discarded skip 2 turns
+        e. Variant - Scoring - add hands of losing players and keep dealing until a players hand exceeds 101
+            - Ace = 1, Face cards = 10, Jokers = 25, number cards = their number value
+            - the player whose hand > 101 is the loser and game ends
+4. Additional items left to do:
+    
+        a. Need more tests
+        b. Calculate for efficiency
+        c. make sure well commented and easy to understand
+        d. use good formatting
--- rummy.py
+++ rummy.py
@@ -6,65 +6,30 @@ import random
 """
 Author: Ben Black
 A simple rummy game demonstrating object oriented design principles.
-
-Background:
-A deck of cards consists of 52 individual cards, divided into
-four sets of thirteen cards. These four sets are known as suits
-and are given names: spades, clubs, hearts, and diamonds.
-With each set, the same set of numbered cards appears,
-running from two to 10, alongside four “special” cards, the
-ace, jack, queen and king, each represented by the first letter
-of their name, as shown below.
-A deck of cards may be used to play a large number of games,
-involving a variety of rules and scoring mechanisms.
-
-Fig 1: An example deck of cards (source: Wikipedia/Wikimedia Commons)
-For this task, we shall make use of the game of rummy. This
-game is typically played with between two and four players.
-Each player is initially dealt seven cards from the deck, the
-remaining cards forming a stock from which additional cards
-may be drawn as the game progresses.
-To win the game, a player must form a hand comprising of
-two “melds” of cards, one of three cards, one of four, both
-consisting of either of:
-• a sequential run of numbers of the same suit, e.g. the
-ace, two, three and four of clubs, or the seven, eight and
-nine of spades.
-• a collection of cards of the same face value from
-differing suits, e.g. all four sixes, or three eights.
-An example of a winning hand for rummy, comprising of the seven to ten of diamonds
-and the aces of clubs, hearts, and spades. (source:
-Wikipedia/Wikimedia Commons)
-
-On each player’s turn, they
-must draw a card either from the draw deck or by taking
-the top card from the discard pile, if one is available.
-The player uses this card to improve his hand, aiming to
-build up a valid meld over a number of turns. To
-
-complete his turn, the player must discard one of his
-cards onto a discard pile, leaving him with exactly seven
-cards in his hand.
 """
 
 #Constants
+FACE = [ 'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King' ]
 SUIT = ['Hearts', 'Clubs', 'Spades', 'Diamonds']
-FACE_VALUES = { 'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13 }
-# JOKER = { 'J*': 777 }
+
+# Not used yet
+JOKERS = { 'Little Joker': 666, 'Big Joker': 777 }
+VALUES = { 'Ace': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13 }
 
 class Card:
     # Class to describe a card
 
-    def __init__(self, suit, value):
+    def __init__(self, face, suit):
         """
         Constructor
         Args: 
             suit: string
-            value: character
+            face: string
         Returns: none
         """
+        self.face = face
         self.suit = suit
-        self.value = value
+        
 		
     def __str__(self):
         """
@@ -72,87 +37,167 @@ class Card:
         Args: none
         Returns: string representation of the card
         """
-        return (self.suit + self.value)
+        return '{0} of {1}'.format(self.face, self.suit)
 
 
 class Deck:
     # Class to describe a deck
 
-    def __init__(self, name):
+    def __init__(self):
         """
         Constructor
-        Args: 
-            deck: number of cards in the deck - int
+        Args: none
         Returns: none
         """
-        self.cards = []  #for cards in the deck
-        self.stack = []  #for the draw stack
-        self.pile = []   #for the discard pile
-        self.hand = []
-        self.name = name
-        
-    def shuffle(self):
-		""" Shuffle the Deck, so that cards are ordered in a random order
-		Args: none
-		Returns: none
-		"""
-		random.shuffle(self.cards)
-		
-    def deal_card(self, card):
-        """ Deal a Card to the Player
-        Args:
-        	card:  The Card object provided to Player as part of the deal
-        Returns:
-        	No returns
-        """
-        try:
-        	self.hand.append(card)
-        	if len(self.hand) > 7:
-        		raise ValueError('ERROR: Player cannot have more than 7 cards during turn')
-        except ValueError as err:
-        	print(err.args) 
-	
-    def draw_cards(self):
+        # talk about optimization here
+        self.deck_stack = []
+        for s in SUIT:
+            for f in FACE:
+                self.deck_stack.append(Card(f, s))
+    
+    def draw_card(self):
         """ Draw a card from the top of the Deck
         Args:
         	No args
         Returns:
         	a Card Object
         """
-        s = self.stack[0]
-        self.cards.pop(0)
+        s = self.deck_stack[0]
+        s.deck_stack.pop(0)
         return s
-
-    def discard(self):
-        """ Discard a card from the players hand
+    
+    def discard(self, discard_pile, card):
+        # Discard a card from the players hand
+        """
         Args:
-        	No args
+        No args
+        Returns:
+        a Card Object
+        """
+        
+        self.discard_pile = []
+        self.card = card
+        
+        p = self.discard_pile[0]
+        self.card.append(0)
+        return p
+        
+    def shuffle(self):
+        """ Shuffle the Deck, so that cards are ordered in a random order
+        Args: none
+        Returns: none
+        """
+        #shuffle algorithm goes here
+        random.shuffle(self.deck_stack)
+
+class Player:
+    #Class to describe a player
+    
+    def __init__(self, name, deck, game_play):
+		""" Class Constructor
+		Args:
+			name: Name of the Player - string
+			deck: Reference to the Deck Object that is part of the Game
+			game: Reference to the Game object that is being played now
+		Returns:
+			No return value
+		"""
+		self.hand = []
+		self.name = name
+		self.deck = deck
+		self.game_play = game_play
+        
+    def deal_card(self, card, hand):
+
+        """ Deal a card from the top of the Deck
+        Args:
+        	deck_stack, number_in_hand
         Returns:
         	a Card Object
         """
-        p = self.pile[0]
-        self.cards.append(0)
-        return p
-"""
+        try:
+            self.hand.append(card)
+            if len(hand) > 7:
+		    raise ValueError('ERROR: Player cannot have more than 14 cards during turn')
+        except ValueError as err:
+		    print(err.args)
+    def choose_discard(self, card):
+        """
+        Args: card: The player chooses to discard 
+		Returns: none
+		"""
+		# Get the card from hand
+        """card = get_object(self.hand, card)"""
+        if card not in self.hand:
+		    return False
+        else:
+		    self.hand.remove(card)
+		    self.deck.discard(card)
+		    return True
+
+class Rummy:
+    # Class to describe rummy game play
+    
+    #need to initialize
+    
+    """
+    Defines playing rummy:
+    1. hand dealt to each player - 2-4
+    2. take turns drawing/improving/discard (max number of cards is 7)
+    3. Need to loop until win condition is met:
+        - sort the hand
+        - loop through hand looking for the following:
+            - check for 3 of a kind or 3 of same suit, first...if not found exit immediately to optimize
+            - 4 of same suit, 3 of same suit = 00
+            - 4 of same suit, run of 3       = 01
+            - run of 4, 3 of same suit       = 10
+            - run of 4, run of 3             = 11
+    4. Variant - Jokers...if discarded skip 2 turns
+    5. Variant - Scoring - add hands of losing players and keep dealing until a players hand exceeds 101
+        - Ace = 1, Face cards = 10, Jokers = 25, number cards = their number value
+        - the player whose hand > 101 is the loser and game ends
+    """
+        
+
+
+    
+    
+    """
+    Items to do:
+    1. Need more tests
+    2. Calculate for efficiency
+    3. easy to understand
+    4. use good formatting
+    """
+
+def main():
+
+    # Create Deck
+    deck = Deck()
+
+    """Test Outputs"""
+    # test all cards in the deck
+    for i in deck.deck_stack:
+        print i
+    # test shuffle (can do conditional formatting duplicate matching in excel to check)
+    print("\n\n")
+    deck.shuffle()
+    for i in deck.deck_stack:
+        print i
+    """
 
 
 
-Client script using classes to play rummy:
-1. hand dealt to each player - 2-4
-2. take turns drawing/improving/discard (max number of cards is 7)
-3. win conditions
-    - 4 of same suit, 3 of same suit = 00
-    - 4 of same suit, run of 3       = 01
-    - run of 4, 3 of same suit       = 10
-    - run of 4, run of 3             = 11
-4. scoring
-"""
+	# New game with 2 players
+	g = Rummy(2, deck)
 
+	# Now let the Players begin
+	g.play()
+	
+	# Joker Logic should go here
+	# deck.set_joker()
+    """
 
-"""
-Items to do:
-1. Need unit tests
-2. Calculate for efficiency
-3. easy to understand
-4. use good formatting
-"""
+if __name__ == "__main__":
+    main()
+   	# unit_tests()
